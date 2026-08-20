af40928745d7aa373bb10d5e64966cbd79a967ce
boynat6060 <boynat6060@ccsdschools.com>
2026-08-20 14:29:45 -0400

    Update script.js

---
+++ script.js
@@ -0,0 +1,17 @@
+let p1;
+let p2;
+let p3;
+let ilt;
+let p4;
+let p5;
+let p6;
+
+const date = new Date();
+const day = date.getDay();
+
+if (day === 3) {
+  p1 = new date();
+  p1.setHours(9,55,0,0);
+}
+else {
+  p1 =
