b90b04df7ce0202667b0967db5984d62d7e5891c
boynat6060 <boynat6060@ccsdschools.com>
2026-08-21 10:03:38 -0400

    js

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-let p1;
-let p2;
-let p3;
-let ilt;
-let p4;
-let p5;
-let p6;
+let p1 = new Date();
+let p2 = new Date();
+let p3 = new Date();
+let ilt = new Date();
+let p4 = new Date();
+let p5 = new Date();
+let p6 = new Date();
 
 const date = new Date();
 const day = date.getDay();
@@ -13,4 +13,5 @@ if (day === 3) {
   p1.setHours(9,55,0,0);
 }
 else {
-  p1 =
+  p1.setHours(10,55,0,0);
+}
