f7a3d7467e615dd292e305f48f097daf095d1c7e
boynat6060 <boynat6060@ccsdschools.com>
2026-08-21 13:35:54 -0400

    times

--- script.js
+++ script.js
@@ -11,9 +11,21 @@ const day = date.getDay();
 
 if (day === 3) {
   p1.setHours(9,55,0,0);
+  p2.setHours(10,40,0,0);
+  p3.setHours(11,45,0,0);
+  ilt.setHours(12,30,0,0);
+  p4.setHours(1,15,0,0);
+  p5.setHours(2,0,0,0);
+  p6.setHours(3,30,0,0);
 }
 else {
-  p1.setHours(10,55,0,0);
+  p1.setHours(9,56,0,0);
+  p2.setHours(10,46,0,0);
+  p3.setHours(11,35,0,0);
+  ilt.setHours(12,20,0,0);
+  p4.setHours(1,10,0,0);
+  p5.setHours(1,59,0,0);
+  p6.setHours(3,30,0,0);
 }
 
 const mainTimeTxt = document.getElementById("mainTimeTxt");
