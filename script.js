f7725459f5114680338d5aef7ce0fae7240fd9aa
boynat6060 <boynat6060@ccsdschools.com>
2026-09-09 21:33:14 -0400

    this is broken brochacho

--- script.js
+++ script.js
@@ -145,4 +145,6 @@ document.getElementById("bellSched").textContent = sched;
 
 let mq1;
 
-document.getElementById("mq1").textContent = mq1;
+document.getElementById("mq1").innerText = "This is some text";
+document.getElementById("mq2").innerText = "This is more text";
+document.getElementById("mq3").innerText = "This is even more text";
